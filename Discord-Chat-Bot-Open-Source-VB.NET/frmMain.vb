30b9eaa43950eb13b863d10c0d80945b442d6797
agentsix1 <spamalotsmileface@gmail.com>
2017-08-31 15:33:30 -0700

    minor changes
    Just adding comments

--- Discord-Chat-Bot-Open-Source-VB.NET/frmMain.vb
+++ Discord-Chat-Bot-Open-Source-VB.NET/frmMain.vb
@@ -1,6 +1,8 @@
 ﻿Public Class frmMain
+    'The my.settings.build will be removed and replaced with a actual build number for a actual release.
     Public ver As String = "DEV build " & My.Settings.Build
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'The next 3 lines of code will only be seen in the development versions. As it would keep count builds if the user ran the program haha.
         My.Settings.Build += 1
         My.Settings.Save()
         ver = "DEV build " & My.Settings.Build
